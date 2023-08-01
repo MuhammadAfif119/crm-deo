310c70ba58119b3446037b1e82d1faf9de4f7a55
ftrianaa <fitriana0116@gmail.com>
2023-08-01 10:25:05 +0700

    newest

--- src/Pages/Listing/ListingPage.jsx
+++ src/Pages/Listing/ListingPage.jsx
@@ -1,6 +1,5 @@
 import { Stack } from '@chakra-ui/react'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import ViewPageListing from './ViewPageListing'
 import AddButtons from '../../Components/Buttons/AddButtons'
 
